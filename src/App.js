0ce3703c5dac1dd308bbced16a03de287ddb9713
erikeggers <em.eggers@gmail.com>
2021-05-11 11:45:19 -0400

    creates login view and adds basic styles for 50/50 split

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Form from './components/form'
+import Login from './login'
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+      <Login/>
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,22 @@
-import React from 'react';
 import styled from 'styled-components';
 import Input from './input'
 
 const Form = () => {
  return (
-  <>
+  <Wrapper>
     <FormHeader>Login</FormHeader>
     <StyledForm>
       <Input name="Email Address" type="email"/>
       <Input name="Password" type="password"/>
     </StyledForm>
-  </>
+  </Wrapper>
  )
 };
  
+
+const Wrapper = styled.div`
+  width: 567px;
+`
 
 const StyledForm = styled.form`
   max-width: 567px;
